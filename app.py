1ece460bc05df4527c38e44ee99b2234c5063585
whyKara <karapurkaryash@gmail.com>
2022-06-12 22:35:13 +0530

    clean main file

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
-from crypt import methods
 from flask import Flask, render_template, request
 import cv2
-import numpy as np
-from matplotlib import pyplot as plt
-import easyocr
 import pytesseract
-from PIL import Image
+
 
 app = Flask(__name__)
 
@@ -21,22 +17,22 @@ def noise_removal(image):
     return (image)
 
 
-# def thin_font(image):
-#     import numpy as np
-#     image = cv2.bitwise_not(image)
-#     kernel = np.ones((2, 2), np.uint8)
-#     image = cv2.erode(image, kernel, iterations=1)
-#     image = cv2.bitwise_not(image)
-#     return (image)
+def thin_font(image):
+    import numpy as np
+    image = cv2.bitwise_not(image)
+    kernel = np.ones((2, 2), np.uint8)
+    image = cv2.erode(image, kernel, iterations=1)
+    image = cv2.bitwise_not(image)
+    return (image)
 
 
-# def thick_font(image):
-#     import numpy as np
-#     image = cv2.bitwise_not(image)
-#     kernel = np.ones((2, 2), np.uint8)
-#     image = cv2.dilate(image, kernel, iterations=1)
-#     image = cv2.bitwise_not(image)
-#     return (image)
+def thick_font(image):
+    import numpy as np
+    image = cv2.bitwise_not(image)
+    kernel = np.ones((2, 2), np.uint8)
+    image = cv2.dilate(image, kernel, iterations=1)
+    image = cv2.bitwise_not(image)
+    return (image)
 
 
 def remove_borders(image):
@@ -54,25 +50,6 @@ def hello_world():
     return render_template('index.html')
 
 
-# @app.route('/easyOCR', methods=['GET', 'POST'])
-# def predict():
-#     imagefile = request.files['imagefile']
-#     imagepath = "./images/" + imagefile.filename
-#     imagefile.save(imagepath)
-
-#     reader = easyocr.Reader(['en'])
-#     result = reader.readtext(imagepath)
-
-#     text = result[0][1]
-#     output = ''
-
-#     for detection in result:
-#         text = detection[1]
-#         output += (text+' ')
-
-#     return render_template('index.html', prediction=output)
-
-
 @app.route('/', methods=['GET', 'POST'])
 def tasser():
     ocr_result = ''
@@ -82,15 +59,13 @@ def tasser():
     imagefile.save(imagepath)
 
     img = cv2.imread(imagepath)
-    inverted_img = cv2.bitwise_not(img)
     grey_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     thresh, im_bw = cv2.threshold(grey_img, 210, 230, cv2.THRESH_BINARY)
 
-    no_noise = noise_removal(im_bw)
-    no_boders = remove_borders(no_noise)
+    image = thick_font(thin_font(remove_borders(noise_removal(im_bw))))
 
-    ocr_result += pytesseract.image_to_string(no_boders)
+    ocr_result += pytesseract.image_to_string(image)
 
     return render_template('index.html', prediction=ocr_result)
 
