6103efa42096025e2eca61cf4c60f0ba5104f07b
whyKara <karapurkaryash@gmail.com>
2022-06-21 12:35:38 +0530

    ipv done

--- app.py
+++ app.py
@@ -1,66 +1,48 @@
 from flask import Flask, render_template, request
 import cv2
-import numpy as np
-import imutils
-from pickle import load
-from keras.models import load_model
+import pytesseract
+
 
 app = Flask(__name__)
-LB = load(open('./model/labels.pkl', 'rb'))
-teat_model = load_model('./model/model_1')
-
-# IMAGE_PATH = './images/'
 
 
-def sort_contours(cnts, method="left-to-right"):
-    reverse = False
-    i = 0
-    if method == "right-to-left" or method == "bottom-to-top":
-        reverse = True
-    if method == "top-to-bottom" or method == "bottom-to-top":
-        i = 1
-    boundingBoxes = [cv2.boundingRect(c) for c in cnts]
-    (cnts, boundingBoxes) = zip(*sorted(zip(cnts, boundingBoxes),
-                                        key=lambda b: b[1][i], reverse=reverse))
-    # return the list of sorted contours and bounding boxes
-    return (cnts, boundingBoxes)
+def noise_removal(image):
+    import numpy as np
+    kernel = np.ones((1, 1), np.uint8)
+    image = cv2.dilate(image, kernel, iterations=1)
+    kernel = np.ones((1, 1), np.uint8)
+    image = cv2.erode(image, kernel, iterations=1)
+    image = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel)
+    image = cv2.medianBlur(image, 3)
+    return (image)
 
 
-def get_letters(img):
-    letters = []
-    image = cv2.imread(img)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    ret, thresh1 = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY_INV)
-    dilated = cv2.dilate(thresh1, None, iterations=2)
-
-    cnts = cv2.findContours(
-        dilated.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cnts = imutils.grab_contours(cnts)
-    cnts = sort_contours(cnts, method="left-to-right")[0]
-    # loop over the contours
-    for c in cnts:
-        if cv2.contourArea(c) > 10:
-            (x, y, w, h) = cv2.boundingRect(c)
-            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        roi = gray[y:y + h, x:x + w]
-        thresh = cv2.threshold(
-            roi, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-        thresh = cv2.resize(thresh, (32, 32), interpolation=cv2.INTER_CUBIC)
-        thresh = thresh.astype("float32") / 255.0
-        thresh = np.expand_dims(thresh, axis=-1)
-        thresh = thresh.reshape(1, 32, 32, 1)
-        ypred = teat_model.predict(thresh)
-        ypred = LB.inverse_transform(ypred)
-        [x] = ypred
-        letters.append(x)
-    return letters, image
-
-# plt.imshow(image)
+def thin_font(image):
+    import numpy as np
+    image = cv2.bitwise_not(image)
+    kernel = np.ones((2, 2), np.uint8)
+    image = cv2.erode(image, kernel, iterations=1)
+    image = cv2.bitwise_not(image)
+    return (image)
 
 
-def get_word(letter):
-    word = "".join(letter)
-    return word
+def thick_font(image):
+    import numpy as np
+    image = cv2.bitwise_not(image)
+    kernel = np.ones((2, 2), np.uint8)
+    image = cv2.dilate(image, kernel, iterations=1)
+    image = cv2.bitwise_not(image)
+    return (image)
+
+
+def remove_borders(image):
+    contours, heiarchy = cv2.findContours(
+        image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cntsSorted = sorted(contours, key=lambda x: cv2.contourArea(x))
+    cnt = cntsSorted[-1]
+    x, y, w, h = cv2.boundingRect(cnt)
+    crop = image[y:y+h, x:x+w]
+    return (crop)
 
 
 @app.route('/', methods=['GET'])
@@ -68,28 +50,25 @@ def hello_world():
     return render_template('index.html')
 
 
-@app.route('/', methods=['POST'])
-def predict():
+@app.route('/', methods=['GET', 'POST'])
+def tasser():
+    ocr_result = ''
 
     imagefile = request.files['imagefile']
     imagepath = "./images/" + imagefile.filename
     imagefile.save(imagepath)
 
-    # reader = easyocr.Reader(['en'])
-    # result = reader.readtext(imagepath)
+    img = cv2.imread(imagepath)
+    grey_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    # text = result[0][1]
-    # output = ''
+    thresh, im_bw = cv2.threshold(grey_img, 210, 230, cv2.THRESH_BINARY)
 
-    # for detection in result:
-    #     text = detection[1]
-    #     output += (text+' ')
-    letter, image = get_letters(imagepath)
-    word = get_word(letter)
+    image = thick_font(thin_font(remove_borders(noise_removal(im_bw))))
 
-    return render_template('index.html', prediction=letter)
+    ocr_result += pytesseract.image_to_string(image)
+
+    return render_template('index.html', prediction=ocr_result)
 
 
 if __name__ == '__main__':
-
     app.run(debug=True)
